23370e3825a5084e0eb6d67e5d09ddd334886730
Maxelx <64378074+maxelx@users.noreply.github.com>
2023-03-02 11:39:34 +0100

    Update WiFiPost.ino

--- WiFiPost.ino
+++ WiFiPost.ino
@@ -1,6 +1,40 @@
+/* Edge Impulse ingestion SDK
+ * Copyright (c) 2022 EdgeImpulse Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
 
+// If your target is limited in memory remove this macro to save 10K RAM
+#define EIDSP_QUANTIZE_FILTERBANK   0
 
+/*
+ ** NOTE: If you run into TFLite arena allocation issue.
+ **
+ ** This may be due to may dynamic memory fragmentation.
+ ** Try defining "-DEI_CLASSIFIER_ALLOCATION_STATIC" in boards.local.txt (create
+ ** if it doesn't exist) and copy this file to
+ ** `<ARDUINO_CORE_INSTALL_PATH>/arduino/hardware/<mbed_core>/<core_version>/`.
+ **
+ ** See
+ ** (https://support.arduino.cc/hc/en-us/articles/360012076960-Where-are-the-installed-cores-located-)
+ ** to find where Arduino installs cores on your machine.
+ **
+ ** If the problem persists then there's not enough memory for this model and application.
+ */
 
+/* Includes ---------------------------------------------------------------- */
+#include <VocapraDef_Merged_Labels_inferencing.h>
+#include <PDM.h>
 #include <ArduinoHttpClient.h>
 #include <WiFiNINA.h>
 #include "arduino_secrets.h"
@@ -23,62 +57,280 @@ HttpClient client = HttpClient( wifi, serverName, port );
 int status = WL_IDLE_STATUS;
 
 
-void setup() 
-{  
-  Serial.begin(9600);
-  while ( status != WL_CONNECTED) {
-    Serial.print("Attempting to connect to Network named: ");
-    Serial.println(ssid);                   // print the network name (SSID);
 
-    // Connect to WPA/WPA2 network:
-    status = WiFi.begin(ssid, pass);
-  }
 
-  // print the SSID of the network you're attached to:
-  Serial.print("SSID: ");
-  Serial.println(WiFi.SSID());
+/** Audio buffers, pointers and selectors */
+typedef struct {
+    int16_t *buffer;
+    uint8_t buf_ready;
+    uint32_t buf_count;
+    uint32_t n_samples;
+} inference_t;
 
-  // print your WiFi shield's IP address:
-  IPAddress ip = WiFi.localIP();
-  Serial.print("IP Address: ");
-  Serial.println(ip);
+static inference_t inference;
+static signed short sampleBuffer[2048];
+static bool debug_nn = false; // Set this to true to see e.g. features generated from the raw signal
+static volatile bool record_ready = false;
+
+/**
+ * @brief      Arduino setup function
+ */
+void setup()
+{
+    // put your setup code here, to run once:
+    Serial.begin(115200);
+    // comment out the below line to cancel the wait for USB connection (needed for native USB)
+    while (!Serial);
+    Serial.println("Edge Impulse Inferencing Demo");
+
+    // summary of inferencing settings (from model_metadata.h)
+    ei_printf("Inferencing settings:\n");
+    ei_printf("\tInterval: ");
+    ei_printf_float((float)EI_CLASSIFIER_INTERVAL_MS);
+    ei_printf(" ms.\n");
+    ei_printf("\tFrame size: %d\n", EI_CLASSIFIER_DSP_INPUT_FRAME_SIZE);
+    ei_printf("\tSample length: %d ms.\n", EI_CLASSIFIER_RAW_SAMPLE_COUNT / 16);
+    ei_printf("\tNo. of classes: %d\n", sizeof(ei_classifier_inferencing_categories) / sizeof(ei_classifier_inferencing_categories[0]));
+
+    if (microphone_inference_start(EI_CLASSIFIER_RAW_SAMPLE_COUNT) == false) {
+        ei_printf("ERR: Could not allocate audio buffer (size %d), this could be due to the window length of your model\r\n", EI_CLASSIFIER_RAW_SAMPLE_COUNT);
+        return;
+    }
+
+    //WIFI
+    while ( status != WL_CONNECTED) {
+      Serial.print("Attempting to connect to Network named: ");
+      Serial.println(ssid);                   // print the network name (SSID);
+
+      // Connect to WPA/WPA2 network:
+      status = WiFi.begin(ssid, pass);
+    }
+
+    // print the SSID of the network you're attached to:
+    Serial.print("SSID: ");
+    Serial.println(WiFi.SSID());
+
+    // print your WiFi shield's IP address:
+    IPAddress ip = WiFi.localIP();
+    Serial.print("IP Address: ");
+    Serial.println(ip);
 
 }
 
-void loop() 
+/**
+ * @brief      Arduino main function. Runs the inferencing loop.
+ */
+void loop()
 {
-  String accuracy = "0.8";
-  String label = "Mangiare";
-  //Composing json
-  Serial.println("making POST request");
-  String a = "{\"Accuracy\": ";
-  String b = ", \"DeviceID\" : ";
-  String c = ", \"Label\": \"";
-  String d = "\", \"Farm\": \"";
-  String out = "\"}";
-  String postData = a+accuracy+b+iccid+c+label+d+farm+out;
+    ei_printf("Starting inferencing in 2 seconds...\n");
 
-  Serial.println(postData);
+    delay(2000);
 
-  client.beginRequest();
-  client.post("/mex");
-  client.sendHeader("Content-Type", "application/json");
-  client.sendHeader("Content-Length", postData.length());
-  client.sendHeader("X-Custom-Header", "custom-header-value");
-  client.beginBody();
-  client.print(postData);
-  client.endRequest();
+    ei_printf("Recording...\n");
 
-  // read the status code and body of the response
-  int statusCode = client.responseStatusCode();
-  String response = client.responseBody();
+    bool m = microphone_inference_record();
+    if (!m) {
+        ei_printf("ERR: Failed to record audio...\n");
+        return;
+    }
 
-  Serial.print("Status code: ");
-  Serial.println(statusCode);
-  Serial.print("Response: ");
-  Serial.println(response);
+    ei_printf("Recording done\n");
 
-  Serial.println("Wait five seconds");
-  delay(5000);
-  
-}  
+    signal_t signal;
+    signal.total_length = EI_CLASSIFIER_RAW_SAMPLE_COUNT;
+    signal.get_data = &microphone_audio_signal_get_data;
+    ei_impulse_result_t result = { 0 };
+
+    EI_IMPULSE_ERROR res = run_classifier_continuous(&signal, &result, debug_nn);
+    if (res != EI_IMPULSE_OK) {
+        ei_printf("ERR: Failed to run classifier (%d)\n", res);
+        return;
+    }
+
+    // print inference return code
+    ei_printf("run_classifier returned: %d\r\n", res);
+    print_inference_result(result);
+}
+
+/**
+ * @brief      PDM buffer full callback
+ *             Copy audio data to app buffers
+ */
+static void pdm_data_ready_inference_callback(void)
+{
+    int bytesAvailable = PDM.available();
+
+    // read into the sample buffer
+    int bytesRead = PDM.read((char *)&sampleBuffer[0], bytesAvailable);
+
+    if ((inference.buf_ready == 0) && (record_ready == true)) {
+
+        for(int i = 0; i < bytesRead>>1; i++) {
+            inference.buffer[inference.buf_count++] = sampleBuffer[i];
+
+            if(inference.buf_count >= inference.n_samples) {
+                inference.buf_count = 0;
+                inference.buf_ready = 1;
+                break;
+            }
+        }
+    }
+}
+
+/**
+ * @brief      Init inferencing struct and setup/start PDM
+ *
+ * @param[in]  n_samples  The n samples
+ *
+ * @return     { description_of_the_return_value }
+ */
+static bool microphone_inference_start(uint32_t n_samples)
+{
+    inference.buffer = (int16_t *)malloc(n_samples * sizeof(int16_t));
+
+    if(inference.buffer == NULL) {
+        return false;
+    }
+
+    inference.buf_count  = 0;
+    inference.n_samples  = n_samples;
+    inference.buf_ready  = 0;
+
+    // configure the data receive callback
+    PDM.onReceive(pdm_data_ready_inference_callback);
+
+    PDM.setBufferSize(2048);
+    delay(250);
+
+    // initialize PDM with:
+    // - one channel (mono mode)
+    if (!PDM.begin(1, EI_CLASSIFIER_FREQUENCY)) {
+        ei_printf("ERR: Failed to start PDM!");
+        microphone_inference_end();
+        return false;
+    }
+
+    // optionally set the gain, defaults to 24
+    // Note: values >=52 not supported
+    //PDM.setGain(40);
+
+    return true;
+}
+
+/**
+ * @brief      Wait on new data
+ *
+ * @return     True when finished
+ */
+static bool microphone_inference_record(void)
+{
+    bool ret = true;
+
+    record_ready = true;
+    while (inference.buf_ready == 0) {
+        delay(10);
+    }
+
+    inference.buf_ready = 0;
+    record_ready = false;
+
+    return ret;
+}
+
+/**
+ * Get raw audio signal data
+ */
+static int microphone_audio_signal_get_data(size_t offset, size_t length, float *out_ptr)
+{
+    numpy::int16_to_float(&inference.buffer[offset], out_ptr, length);
+
+    return 0;
+}
+
+/**
+ * @brief      Stop PDM and release buffers
+ */
+static void microphone_inference_end(void)
+{
+    PDM.end();
+    ei_free(inference.buffer);
+}
+
+void print_inference_result(ei_impulse_result_t result) {
+
+    // Print how long it took to perform inference
+    ei_printf("Timing: DSP %d ms, inference %d ms, anomaly %d ms\r\n",
+            result.timing.dsp,
+            result.timing.classification,
+            result.timing.anomaly);
+    
+    String massi = "";
+    float massimo = 0.0;
+
+    ei_printf("Predictions:\r\n");
+    for (uint16_t i = 0; i < EI_CLASSIFIER_LABEL_COUNT; i++) {
+        //ei_printf("  %s: ", ei_classifier_inferencing_categories[i]);
+        //ei_printf("%.5f\r\n", result.classification[i].value);
+        if(massimo< result.classification[i].value){
+          massimo = result.classification[i].value;
+          massi = String(ei_classifier_inferencing_categories[i]);
+          //ei_printf("massimo\n");
+        }        
+    }
+
+    //SEND 
+
+    Serial.println(massi);
+    Serial.println(massimo);
+    Serial.println("");
+
+    //String accuracy = "0.8";
+    //String label = "Mangiare";
+
+    String accuracy = String(massimo);
+    String label = massi;
+
+    //Composing json
+    Serial.println("making POST request");
+    String a = "{\"Accuracy\": ";
+    String b = ", \"DeviceID\" : ";
+    String c = ", \"Label\": \"";
+    String d = "\", \"Farm\": \"";
+    String out = "\"}";
+    String postData = a+accuracy+b+iccid+c+label+d+farm+out;
+
+    Serial.println(postData);
+
+    client.beginRequest();
+    client.post("/mex");
+    client.sendHeader("Content-Type", "application/json");
+    client.sendHeader("Content-Length", postData.length());
+    client.sendHeader("X-Custom-Header", "custom-header-value");
+    client.beginBody();
+    client.print(postData);
+    client.endRequest();
+
+    // read the status code and body of the response
+    int statusCode = client.responseStatusCode();
+    String response = client.responseBody();
+
+    Serial.print("Status code: ");
+    Serial.println(statusCode);
+    Serial.print("Response: ");
+    Serial.println(response);
+
+    Serial.println("Wait five seconds");
+    delay(5000);
+
+
+
+    // Print anomaly result (if it exists)
+#if EI_CLASSIFIER_HAS_ANOMALY == 1
+    ei_printf("Anomaly prediction: %.3f\r\n", result.anomaly);
+#endif
+
+}
+
+#if !defined(EI_CLASSIFIER_SENSOR) || EI_CLASSIFIER_SENSOR != EI_CLASSIFIER_SENSOR_MICROPHONE
+#error "Invalid model for current sensor."
+#endif
